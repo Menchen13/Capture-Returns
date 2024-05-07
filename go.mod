331a097c4a7c5e86a587c7a9caf916213a038c93
Nik Hildebrandt <nik.hildebrandt13@gmail.com>
2024-05-07 16:28:39 +0200

    Added tesseract OCR for Term analysis in pictures .go.

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module Menchen13/Capture-Returns
 
 go 1.21.5
+
+require (
+	github.com/otiai10/gosseract v2.2.1+incompatible // indirect
+	github.com/otiai10/mint v1.6.3 // indirect
+	golang.org/x/net v0.25.0 // indirect
+)
---
+++ pictures.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+
+	"github.com/otiai10/gosseract"
+)
+
+func string() ([]byte, error) {
+	client := gosseract.NewClient()
+	defer client.Close()
+
+	//getting testing image string from Responses
+	str, err := base64.StdEncoding.DecodeString("")
+	if err != nil {
+		return []byte{}, err
+	}
+
+	client.SetWhitelist("0123456789+*-/")
+	client.Trim = true
+	client.SetImageFromBytes(str)
+	fmt.Println(client.Text())
+}
