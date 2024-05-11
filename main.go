422065d1bf025d15f47dc411e3b0f5b4801a7561
Nik Hildebrandt <nik.hildebrandt13@gmail.com>
2024-05-11 16:54:09 +0200

    added l and p Flags

--- main.go
+++ main.go
@@ -3,15 +3,26 @@ package main
 
 import (
 	"flag"
+	"fmt"
 )
 
 func main() {
 
 	url := flag.String("u", "", "Url to attack")
+	userfile := flag.String("l", "", "Path to file with usernames")
+	passfile := flag.String("p", "", "Path to file with passwords")
 	flag.Parse()
 	if *url == "" {
+		fmt.Println("No Url specified")
 		flag.PrintDefaults()
 		return
+	} else if *userfile == "" {
+		fmt.Println("No userfile specified")
+		flag.PrintDefaults()
+		return
+	} else if *passfile == "" {
+		fmt.Println("No passfile specified")
+		flag.PrintDefaults()
 	}
 
 	//client := captcha.Sync(*url)
