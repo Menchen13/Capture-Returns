0dd9d36110836935bb3029610bf933654568fe08
Nik Hildebrandt <nik.hildebrandt13@gmail.com>
2024-05-19 19:46:01 +0200

    .

--- brute/brute_test.go
+++ brute/brute_test.go
@@ -9,5 +9,6 @@ import (
 func Test_try(t *testing.T) {
 	r := try(http.DefaultClient, "http://10.10.122.216/login", "test1", "test2")
 
-	fmt.Println(r)
+	var arr []byte = make([]byte, r.ContentLength)
+	fmt.Println(string(arr))
 }
