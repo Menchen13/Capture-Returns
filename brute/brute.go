6f3c2403afa4296eb228e6709d4509c41c94396c
Nik Hildebrandt <nik.hildebrandt13@gmail.com>
2024-05-19 20:16:00 +0200

    retest try

--- brute/brute.go
+++ brute/brute.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
-	"strings"
 )
 
 func Try(client *http.Client, u string, user string, pass string) *http.Response {
@@ -12,9 +11,10 @@ func Try(client *http.Client, u string, user string, pass string) *http.Response
 	v.Set("username", user)
 	v.Set("password", pass)
 	//finish url if needed
-	if !strings.Contains(u, "login") {
-		u += "/login"
-	}
+	/*
+		if !strings.Contains(u, "login") {
+			u += "/login"
+		}*/
 
 	resp, err := client.PostForm(u, v)
 	if err != nil {
