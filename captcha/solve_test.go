18aaadbc6cd37459880a3861eaa81d3b4e8c8825
Nik Hildebrandt <nik.hildebrandt13@gmail.com>
2024-05-27 14:07:16 +0200

    added solver test

--- captcha/solve_test.go
+++ captcha/solve_test.go
@@ -36,3 +36,25 @@ func Test_getImage(t *testing.T) {
 		})
 	}
 }
+
+func TestSolver(t *testing.T) {
+	type args struct {
+		u string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+		{
+			name: "Solver Test",
+			//this address is temporary
+			args: args{u: "http://10.10.11.28"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			Solver(tt.args.u)
+		})
+	}
+}
