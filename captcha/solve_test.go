79bf3f30000e54a6782b6e0f12184d3298a7d95e
Nik Hildebrandt <nik.hildebrandt13@gmail.com>
2024-07-09 21:53:10 +0200

    getting ready for another solver test

--- captcha/solve_test.go
+++ captcha/solve_test.go
@@ -19,7 +19,7 @@ func TestSolver(t *testing.T) {
 		{
 			name: "Solver Test",
 			//this address is temporary
-			args: args{u: "http://10.10.251.61"},
+			args: args{u: "http://10.10.228.41"},
 		},
 	}
 	for _, tt := range tests {
