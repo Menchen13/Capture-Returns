b4ad1b2a6b7be3c69dc59830178e6e67d9dad36d
Nik Hildebrandt <nik.hildebrandt13@gmail.com>
2024-07-13 20:40:04 +0200

    trying new shape detection using html parsing

--- captcha/detect.go
+++ captcha/detect.go
@@ -14,29 +14,30 @@ func isShape(resp *http.Response) bool {
 	//read in response Body
 	BytesBody, err := io.ReadAll(resp.Body)
 	if err != nil {
-		panic(fmt.Errorf("Error Reading in Body: %w", err))
+		panic(fmt.Errorf("error reading in body: %w", err))
 	}
 	defer resp.Body.Close()
 
 	doc, err := html.Parse(bytes.NewReader(BytesBody))
 
+	if err != nil {
+		panic(fmt.Errorf("error parsing: '%w'", err))
+	}
+
+	var shape bool = false
 	var f func(*html.Node)
 	f = func(n *html.Node) {
-		//
-		if n.Type == html.ElementNode && n.Data == "a" {
-			for _, a := range n.Attr {
-				if a.Key == "href" {
-					fmt.Println(a.Val)
-					break
-				}
-			}
+		// fmt.Println("Type: ", n.Type, "Data: ", n.Data, "Namespace: ", n.Namespace, "Attributes: ", n.Attr)
+		if n.Type == 1 && n.Data == "Describe the shape below (circle, square, or triangle)" {
+
+			shape = true
 		}
 		for c := n.FirstChild; c != nil; c = c.NextSibling {
-
 			f(c)
 		}
 	}
 	f(doc)
 
-	return false
+	return shape
+
 }
